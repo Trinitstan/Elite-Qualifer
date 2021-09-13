60c5137e8dff98c7cc27a22502ccfd8e8bde66de
Trinity St Ann <90357923+Trinitstan@users.noreply.github.com>
2021-09-13 22:26:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Chatbot section
+
+
+def init_convo():
+  #quit_character ='q'
+  user_name = input("Hello user, what is your name?\n") 
+  response(user_name)
+
+
+  #while user_input != quit_character:
+def response(user_name):
+  print("Hi " {+ user_name + ", How has your day been?")
+  responses = }
+
+if __name__ == "__main__": 
+  init_convo()
