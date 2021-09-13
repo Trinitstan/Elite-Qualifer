03900d3b2cf75ae14a0f4869eab52adbcaf6df8b
Trinity St Ann <90357923+Trinitstan@users.noreply.github.com>
2021-09-13 22:29:24 +0000

    Everything was commented out.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #Chatbot section
 
 
-def init_convo():
+#def init_convo():
   #quit_character ='q'
-  user_name = input("Hello user, what is your name?\n") 
-  response(user_name)
+  #user_name = input("Hello user, what is your name?\n")
+  #response(user_name)
 
 
   #while user_input != quit_character:
-def response(user_name):
-  print("Hi " {+ user_name + ", How has your day been?")
-  responses = }
+#def response(user_name):
+  #print("Hi " {+ user_name + ", How has your day been?")
+  #responses = }
 
-if __name__ == "__main__": 
-  init_convo()
+#if __name__ == "__main__": 
+  #init_convo()
